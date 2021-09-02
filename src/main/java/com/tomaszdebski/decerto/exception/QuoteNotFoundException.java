66e8dc0a4208e6e29c32f9c70e93c1d32d8c959f
Tomek <tsuisou1@gmail.com>
2021-09-02 12:37:39 +0200

    Decerto application commit

--- src/main/java/com/tomaszdebski/decerto/exception/QuoteNotFoundException.java
+++ src/main/java/com/tomaszdebski/decerto/exception/QuoteNotFoundException.java
@@ -3,6 +3,6 @@ package com.tomaszdebski.decerto.exception;
 public class QuoteNotFoundException extends RuntimeException{
 
     public QuoteNotFoundException(Long id) {
-        super("Could not find employee " + id);
+        super("Could not find quote " + id);
     }
 }
